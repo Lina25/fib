ed0fb192b0ca3dafc614445602af16c907214be1
Lina <lina.kurylo@gmail.com>
2016-04-25 11:47:47 +0000

    55

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-var n = 10; // Сколько элементов хотим получить
-var fibonacci = [0, 1]; // Первые два элемента последовательности Фибоначчи
+var i;
+var fibonacci = [0, 1];
 
-for (i = 2; i < n; i ++) {
-  // Получаем i-й элемент последовательности как сумму предыдущих двух
+for (i = 2; i < 30; i ++) {
   fibonacci[i] = fibonacci[i-1] + fibonacci[i-2];
 }
 
-console.log(fibonacci.slice(0,n));
+console.log(fibonacci.slice(0,30));
 
